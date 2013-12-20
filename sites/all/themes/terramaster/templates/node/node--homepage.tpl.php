38559741faa38e31a8de1247fc3e9fb80b29690d
pbonnefoi <pitair23@gmail.com>
2013-12-20 08:22:59 +0100

    commit of the year

--- sites/all/themes/terramaster/templates/node/node--homepage.tpl.php
+++ sites/all/themes/terramaster/templates/node/node--homepage.tpl.php
@@ -25,13 +25,7 @@
 
   <div<?php print $content_attributes; ?>>
     <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
       print render($content);
     ?>
   </div>
-
-  <?php print render($content['links']); ?>
-  <?php print render($content['comments']); ?>
 </article>
